231f3995303494394b197d9cc2e301332e520061
Ravinani5689 <rteja2257@gmail.com>
2026-07-11 11:10:05 +0530

    11-07-2026we

--- src/com/ravi/studentmanagement/model/Student.java
+++ src/com/ravi/studentmanagement/model/Student.java
@@ -40,7 +40,7 @@ public class Student {
     public String getName() {
         return name;
     }
-
+    //ds
     public void setName(String name) {
         if (name == null || name.trim().isEmpty()) {
             throw new IllegalArgumentException("Name cannot be empty.");
